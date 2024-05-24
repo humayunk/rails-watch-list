9bad5d832eb64b5048728e71770375721207eed0
humayun khan <humayun.n.k@gmail.com>
2024-05-24 16:38:40 -0400

    added styling

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,6 +1,7 @@
 class List < ApplicationRecord
   has_many :bookmarks, dependent: :destroy
   has_many :movies, through: :bookmarks
+  has_one_attached :photo
 
   #ensure the name is present and unique
   validates :name, presence: true, uniqueness: true
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,41 +1,61 @@
-# Clear existing data
-Bookmark.delete_all
+# Clear existing movie-related data
+Bookmark.where(movie_id: Movie.pluck(:id)).delete_all
 Movie.delete_all
 List.delete_all
 
-# Create Lists
-lists = [
-  { name: 'Drama' },
-  { name: 'All time favourites' },
-  { name: 'Girl Power' }
-]
+# # Create Lists (if they don't already exist)
+# lists = [
+#   { name: 'Drama' }
+# ]
 
-lists.each do |list|
-  List.create!(list)
-end
+# lists.each do |list|
+#   List.find_or_create_by!(list)
+# end
 
 # Create Movies
-movies = [
-  { title: 'Wonder Woman 1984', overview: 'Wonder Woman comes into conflict with the Soviet Union during the Cold War in the 1980s.', poster_url: 'https://image.tmdb.org/t/p/original/8UlWHLMpgZm9bx6QYh0NFoq67TZ.jpg', rating: 6.9 },
-  { title: 'The Shawshank Redemption', overview: 'Framed in the 1940s for double murder, upstanding banker Andy Dufresne begins a new life at the Shawshank prison.', poster_url: 'https://image.tmdb.org/t/p/original/q6y0Go1tsGEsmtFryDOJo3dEmqu.jpg', rating: 8.7 },
-  { title: 'Titanic', overview: '101-year-old Rose DeWitt Bukater tells the story of her life aboard the Titanic.', poster_url: 'https://image.tmdb.org/t/p/original/9xjZS2rlVxm8SFx8kPC3aIGCOYQ.jpg', rating: 7.9 },
-  { title: 'The Green Mile', overview: 'A supernatural tale set on death row in a Southern prison.', poster_url: 'https://image.tmdb.org/t/p/original/velWPhVMQeQKcxggNEU8Ymlo52R.jpg', rating: 8.5 },
-  { title: 'Ocean\'s Eight', overview: 'Debbie Ocean, a criminal mastermind, gathers a crew of female thieves to pull off the heist of the century.', poster_url: 'https://image.tmdb.org/t/p/original/MvYpKlpFivIivhEPmY1rPsddqHO.jpg', rating: 7.0 }
-]
+require 'net/http'
+require 'json'
+
+API_KEY = 'd238b4128c2101bc306d0a254501ba88'
+BASE_URL = 'https://api.themoviedb.org/3'
+
+def fetch_movies(page)
+  url = "#{BASE_URL}/movie/popular?api_key=#{API_KEY}&language=en-US&page=#{page}"
+  uri = URI(url)
+  response = Net::HTTP.get(uri)
+  JSON.parse(response)['results']
+end
+
+def movie_data(movie)
+  return if movie['overview'].blank? || movie['poster_path'].blank?
+
+  {
+    title: movie['title'],
+    overview: movie['overview'],
+    poster_url: "https://image.tmdb.org/t/p/original/#{movie['poster_path']}",
+    rating: movie['vote_average']
+  }
+end
+
+movies = []
+(1..5).each do |page|
+  movies += fetch_movies(page).map { |movie| movie_data(movie) }.compact
+end
 
 movies.each do |movie|
+  next if Movie.exists?(title: movie[:title])
   Movie.create!(movie)
 end
 
-# Create Bookmarks
-bookmarks = [
-  { comment: 'Recommended by John', movie: Movie.find_by(title: 'Titanic'), list: List.find_by(name: 'All time favourites') },
-  { comment: 'Superhero movie revisited in 2020', movie: Movie.find_by(title: 'Wonder Woman 1984'), list: List.find_by(name: 'Girl Power') },
-  { comment: 'Spielberg\'s masterly Oscar-winning drama', movie: Movie.find_by(title: 'The Shawshank Redemption'), list: List.find_by(name: 'Drama') },
-  { comment: '2020 release', movie: Movie.find_by(title: 'Ocean\'s Eight'), list: List.find_by(name: 'Drama') },
-  { comment: 'Based on Stephen King\'s 1996 novel', movie: Movie.find_by(title: 'The Green Mile'), list: List.find_by(name: 'All time favourites') }
-]
+# # Create Bookmarks
+# bookmarks = [
+#   { comment: 'Recommended by John', movie: Movie.find_by(title: 'Titanic'), list: List.find_by(name: 'All time favourites') },
+#   { comment: 'Superhero movie revisited in 2020', movie: Movie.find_by(title: 'Wonder Woman 1984'), list: List.find_by(name: 'Girl Power') },
+#   { comment: 'Spielberg\'s masterly Oscar-winning drama', movie: Movie.find_by(title: 'The Shawshank Redemption'), list: List.find_by(name: 'Drama') },
+#   { comment: '2020 release', movie: Movie.find_by(title: 'Ocean\'s Eight'), list: List.find_by(name: 'Drama') },
+#   { comment: 'Based on Stephen King\'s 1996 novel', movie: Movie.find_by(title: 'The Green Mile'), list: List.find_by(name: 'All time favourites') }
+# ]
 
-bookmarks.each do |bookmark|
-  Bookmark.create!(bookmark)
-end
+# bookmarks.each do |bookmark|
+#   Bookmark.create!(bookmark)
+# end
